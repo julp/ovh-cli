efd89723aa5179d1bad4b9dd0f1befa2f290559a
julp <poinsot.julien@gmail.com>
2014-10-30 17:33:59 +0100

    divide code by logical functions into sub blocks

--- commands/domain.c
+++ commands/domain.c
@@ -7,6 +7,11 @@
 #include "modules/api.h"
 #include "struct/hashtable.h"
 
+typedef struct {
+    bool uptodate;
+    HashTable *records;
+} domain_t;
+
 typedef enum {
     RECORD_TYPE_ANY, /* special value to target any type */
     RECORD_TYPE_A,
@@ -230,16 +235,19 @@ static int record_list(int argc, const char **argv)
             request_execute(req, RESPONSE_XML, (void **) &doc);
             request_dtor(req);
             // result
+/* TODO */
+/* <DRY> */
             if (NULL == (root = xmlDocGetRootElement(doc))) {
                 return 0;
             }
             r = mem_new(*r);
             r->id = xmlGetPropAsInt(root, "id");
             r->ttl = xmlGetPropAsInt(root, "ttl");
-            r->name = xmlGetPropAsString(root, BAD_CAST "subDomain");
-            r->target = xmlGetPropAsString(root, BAD_CAST "target");
+            r->name = xmlGetPropAsString(root, "subDomain");
+            r->target = xmlGetPropAsString(root, "target");
             r->type = xmlGetPropAsRecordType(root, "fieldType");
             hashtable_quick_put_ex(records, 0, r->id, NULL, r, NULL);
+/* </DRY> */
 #if 0
             for (a = root->properties; a != NULL; a = a->next) {
                 // TODO: build a cache name (+ type ?) => id
@@ -290,9 +298,6 @@ static bool str_include(const char *search, ...)
 static int record_add(int argc, const char **argv)
 {
     xmlDocPtr doc;
-    request_t *req;
-    String *buffer;
-    char *p, url[1024];
     const char *target, *type, *subdomain;
 
     assert(7 == argc);
@@ -303,38 +308,70 @@ static int record_add(int argc, const char **argv)
         fprintf(stderr, "unknown DNS record type '%s'\n", type);
         return 0;
     }
-    // forge url
-    *url = '\0';
-    p = stpcpy(url, API_BASE_URL "/domain/zone/");
-    p = stpcpy(p, argv[0]);
-    p = stpcpy(p, "/record");
-    // data
     {
-        json_value_t root;
-        json_document_t *doc;
+        String *buffer;
 
-        buffer = string_new();
-        doc = json_document_new();
-        root = json_object();
-        json_object_set_property(root, "target", json_string(target));
-        json_object_set_property(root, "fieldType", json_string(type));
-//         if ('\0' != *subdomain)
-            json_object_set_property(root, "subDomain", json_string(subdomain));
-        json_document_set_root(doc, root);
-        json_document_serialize(doc, buffer);
-        json_document_destroy(doc);
+        // data
+        {
+            json_value_t root;
+            json_document_t *doc;
+
+            buffer = string_new();
+            doc = json_document_new();
+            root = json_object();
+            json_object_set_property(root, "target", json_string(target));
+            json_object_set_property(root, "fieldType", json_string(type));
+//             if ('\0' != *subdomain)
+                json_object_set_property(root, "subDomain", json_string(subdomain));
+            json_document_set_root(doc, root);
+            json_document_serialize(doc, buffer);
+            json_document_destroy(doc);
+        }
+        // request
+        {
+            request_t *req;
+            char *p, url[1024];
+
+            // forge url
+            *url = '\0';
+            p = stpcpy(url, API_BASE_URL "/domain/zone/");
+            p = stpcpy(p, argv[0]);
+            p = stpcpy(p, "/record");
+            // request
+            req = request_post(url, buffer->ptr, STRING_NOCOPY);
+            request_add_header(req, "Accept: text/xml");
+            request_add_header(req, "Content-type: application/json");
+            request_sign(req);
+            request_execute(req, RESPONSE_XML, (void **) &doc);
+            request_dtor(req);
+            string_destroy(buffer);
+        }
     }
-    // request
-    req = request_post(url, buffer->ptr, STRING_NOCOPY);
-    request_add_header(req, "Accept: text/xml");
-    request_add_header(req, "Content-type: application/json");
-    request_sign(req);
-    request_execute(req, RESPONSE_XML, (void **) &doc);
-    request_dtor(req);
-    string_destroy(buffer);
     // result
-    // <opt id="\d+" fieldType="CNAME" subDomain="toto" target="www" ttl="0" zone="domain.ext" />
-    // TODO: update cache with response data
+    {
+        record_t *r;
+        xmlNodePtr root;
+        HashTable *records;
+
+        if (!hashtable_get(domains, (void *) argv[0], (void **) &records)) {
+            hashtable_put(domains, (void *) argv[0], records = hashtable_new(NULL, value_equal, NULL, NULL, record_destroy), NULL);
+        }
+/* TODO */
+/* <DRY> */
+        if (NULL == (root = xmlDocGetRootElement(doc))) {
+            return 0;
+        }
+        r = mem_new(*r);
+        r->id = xmlGetPropAsInt(root, "id");
+        r->ttl = xmlGetPropAsInt(root, "ttl");
+        r->name = xmlGetPropAsString(root, "subDomain");
+        r->target = xmlGetPropAsString(root, "target");
+        r->type = xmlGetPropAsRecordType(root, "fieldType");
+        hashtable_quick_put_ex(records, 0, r->id, NULL, r, NULL);
+/* </DRY> */
+    }
+
+    return TRUE;
 }
 
 static int record_delete(int argc, const char **argv)
