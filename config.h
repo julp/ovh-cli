37ff4178ea3aff8c12c82885d215d84eeb485b7a
julp <poinsot.julien@gmail.com>
2014-11-01 18:16:21 +0100

    do not have to be versionned

--- config.h
+++ config.h
@@ -1,3 +1,13 @@
+#ifndef CONFIG_H
+
+#define CONFIG_H
+
 #define APPLICATION_KEY "p78lHft5yNVCK3MX"
 #define APPLICATION_SECRET "J4bln7ApBZ2pDvFDK53AjMenSa1l3kKZ"
 #define API_BASE_URL "https://eu.api.ovh.com/1.0"
+#define WITH_NLS
+#define DEBUG
+#define WITH_EDITLINE
+#define LOCALEDIR "/home/julp/ovh-cli/share/locale/"
+
+#endif /* !CONFIG_H */
