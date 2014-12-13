dd37e406abdff6dc453ebe7a41a25536c2e9cc8e
julp <poinsot.julien@gmail.com>
2014-12-13 19:50:31 +0100

    forgot to commit this file

---
+++ modules/conv.h
@@ -0,0 +1,11 @@
+#ifndef CONV_H
+
+# define CONV_H
+
+void convert_array_free(int, char **, char **);
+bool convert_array_local_to_utf8(int, char **, char ***, error_t **);
+void convert_string_free(const char *, char **);
+bool convert_string_local_to_utf8(const char *, size_t, char **, size_t *, error_t **);
+bool convert_string_utf8_to_local(const char *, size_t, char **, size_t *, error_t **);
+
+#endif /* !CONV_H */
